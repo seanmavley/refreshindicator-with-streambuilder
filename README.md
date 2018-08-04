2712b24f5ccaebbba298bbc34317eb5c3b178616
Rexford <seanmavley@gmail.com>
2018-08-04 22:34:20 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Using RefreshIndicator with StreamBuilder
+
+This project shows how to use `RefreshIndicator` in Flutter with the `StreamBuilder`
+
+The use case is you want upon user swipe down in app, fetch new content from endpoint, and rebuild the `ListView` seamlessly.
+
+If that's what you want, Welcome!
+
+## How to Run
+
+ - Clone this repository
+ - Change into root directory
+ - `flutter run` (I assume your system is Flutter-ready)
---
+++ lib/main.dart
@@ -0,0 +1,137 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:async';
+import 'dart:convert';
+
+void main() => runApp(new MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Basic Project',
+      home: new MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => new _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  StreamController _postsController;
+  final GlobalKey<ScaffoldState> scaffoldKey = new GlobalKey<ScaffoldState>();
+
+  int count = 1;
+
+  Future fetchPost([howMany = 5]) async {
+    final response = await http.get(
+        'https://blog.khophi.co/wp-json/wp/v2/posts/?per_page=$howMany&context=embed');
+
+    if (response.statusCode == 200) {
+      return json.decode(response.body);
+    } else {
+      throw Exception('Failed to load post');
+    }
+  }
+
+  loadPosts() async {
+    fetchPost().then((res) async {
+      _postsController.add(res);
+      return res;
+    });
+  }
+
+  showSnack() {
+    return scaffoldKey.currentState.showSnackBar(
+      SnackBar(
+        content: Text('New content loaded'),
+      ),
+    );
+  }
+
+  Future<Null> _handleRefresh() async {
+    count++;
+    print(count);
+    fetchPost(count * 5).then((res) async {
+      _postsController.add(res);
+      showSnack();
+      return null;
+    });
+  }
+
+  @override
+  void initState() {
+    _postsController = new StreamController();
+    loadPosts();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      key: scaffoldKey,
+      appBar: new AppBar(
+        title: new Text('StreamBuilder'),
+        actions: <Widget>[
+          IconButton(
+            tooltip: 'Refresh',
+            icon: Icon(Icons.refresh),
+            onPressed: _handleRefresh,
+          )
+        ],
+      ),
+      body: StreamBuilder(
+        stream: _postsController.stream,
+        builder: (BuildContext context, AsyncSnapshot snapshot) {
+          print('Has error: ${snapshot.hasError}');
+          print('Has data: ${snapshot.hasData}');
+          print('Snapshot Data ${snapshot.data}');
+
+          if (snapshot.hasError) {
+            return Text(snapshot.error);
+          }
+
+          if (snapshot.hasData) {
+            return Column(
+              children: <Widget>[
+                Expanded(
+                  child: Scrollbar(
+                    child: RefreshIndicator(
+                      onRefresh: _handleRefresh,
+                      child: ListView.builder(
+                        physics: const AlwaysScrollableScrollPhysics(),
+                        itemCount: snapshot.data.length,
+                        itemBuilder: (context, index) {
+                          var post = snapshot.data[index];
+                          return ListTile(
+                            title: Text(post['title']['rendered']),
+                            subtitle: Text(post['date']),
+                          );
+                        },
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            );
+          }
+
+          if (snapshot.connectionState != ConnectionState.done) {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+
+          if (!snapshot.hasData &&
+              snapshot.connectionState == ConnectionState.done) {
+            return Text('No Posts');
+          }
+        },
+      ),
+    );
+  }
+}
